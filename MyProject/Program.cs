9492722f2b534c6bb46bbb7947141a07ec5cfca0
Christopher <cjfg00gl3@gmail.com>
2024-08-31 16:07:11 -0400

    UPDATE Program.cs with the base commands for the command line based program

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CommandLineApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Christopher J. Francisco's command-line app!");
+            string availableCommands = "Available commands: help, quit, holy-cow";
+            string holy_cow = @"
+ ________
+< MOOOO! >
+ ‾‾‾‾‾‾‾‾\     ╭───╮
+          \    ╰───╯
+           \   ^__^
+            \  (oo)\_______
+               (__)\       )\/\
+                   ||----w |
+                   ||     ||";
+            Console.WriteLine(availableCommands);
+            while (true)
+            {
+                
+                string input = Console.ReadLine();
+                switch (input.ToLower())
+                {
+                    case "help":
+                        Console.WriteLine(availableCommands);
+                        break;
+                    case "quit":
+                        Console.WriteLine("x_x");
+                        return;
+                    case "holy-cow":
+                        Console.WriteLine(holy_cow);
+                        break;
+                    default:
+                        Console.WriteLine("Type 'help'.");
+                        break;
+                }
+            }
+
+        }
+    }
+}
