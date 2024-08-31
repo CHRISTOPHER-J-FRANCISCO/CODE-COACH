b4803bf64226319597c834c71c9ab278f9b6db57
Christopher <cjfg00gl3@gmail.com>
2024-08-31 15:28:22 -0400

    CREATE new branch COMMAND-LINE-BASED

--- CODE-COACH/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace CODE_COACH.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- README.md
+++
@@ -1,4 +0,0 @@
-# CODE COACH
-
-- at the moment contains the initial base structure for the mvc
-- at the moment only features the command line based version
