a44c1d00911ad406daa6501547b3f548363ec8ae
Christopher <cjfg00gl3@gmail.com>
2024-08-31 22:58:06 -0400

    UPDATE FileManager.cs to allow the user to edit an existing file in SnippetsDirectory

--- MyProject/FileManager.cs
+++ MyProject/FileManager.cs
@@ -1,40 +1,28 @@
 using System;
 using System.IO;
 
-namespace CommandLineApp 
+namespace CommandLineApp
 {
 
-    public class FileManager 
+    public class FileManager
     {
-
-        private string fileName = "";
-
-        public void setFileName(string newFileName) {
-
-        }
-
-        private string fileContents = "";
-
-        public void setFileContents(string newFileContents) {
-
-        }
-
-        public String getFileContents() {
-            return this.fileContents;
-        }
-
         private static string folderPath = "SnippetsDirectory";
 
+        // Ensures the folder exists, if not creates the folder path
         private static void EnsureFolderExists()
         {
-            if (!Directory.Exists(folderPath)) {
+            if (!Directory.Exists(folderPath))
+            {
                 Console.WriteLine("Directory doesn't exist! Directory created!");
                 Directory.CreateDirectory(folderPath);
-            } else {
+            }
+            else
+            {
                 Console.WriteLine("Directory found!");
             }
         }
 
+        // Creates a file given the file name path
         public static void CreateFile(string fileName)
         {
             EnsureFolderExists();
@@ -42,29 +30,33 @@ namespace CommandLineApp
             Console.WriteLine(folderPath + "/" + fileName + " created!");
         }
 
-        public static void ActuallyDisplayFile(string filePath) {
+        // actually displays the content of the file name path
+        private static void ActuallyDisplayFile(string filePath)
+        {
             Console.Clear();
             int numDashes = 50;
-            Console.WriteLine(new string('-', numDashes) + " " + filePath + " " + new string('-', numDashes));
+            Console.WriteLine(new string('-', numDashes) + " Displaying: " + filePath + " " + new string('-', numDashes));
             using (StreamReader reader = new StreamReader(filePath))
             {
                 string fileContents = reader.ReadToEnd();
                 Console.WriteLine(fileContents);
             }
+            Console.WriteLine();
         }
 
+        // prompts the user to select which pile to display, otherwise the user is warned
         public static void DisplayFile()
         {
             try
             {
-                foreach(string file in Directory.EnumerateFiles(folderPath)) 
+                foreach (string file in Directory.EnumerateFiles(folderPath))
                 {
                     Console.WriteLine(file);
                 }
                 Console.Write("Enter file name (to display): ");
                 string input = Console.ReadLine();
                 bool fileFound = false;
-                foreach(string file in Directory.EnumerateFiles(folderPath))
+                foreach (string file in Directory.EnumerateFiles(folderPath))
                 {
                     if (Path.GetFileName(file) == input)
                     {
@@ -77,11 +69,83 @@ namespace CommandLineApp
                     Console.Write(input + " not found!");
                 }
             }
-            catch(DirectoryNotFoundException)
+            catch (DirectoryNotFoundException)
             {
                 Console.WriteLine("Error finding directory!");
             }
-            catch(IOException)
+            catch (IOException)
+            {
+                Console.WriteLine("Error reading directory!");
+            }
+        }
+
+        private static void ActuallyEditFile(string filePath)
+        {
+            // Display content header 
+            Console.Clear();
+            int numDashes = 50;
+            // Display content file
+            string fileContents = File.ReadAllText(filePath);
+            // Display editing file
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Type changes, backspace to delete, and esc to save.!");
+                Console.WriteLine(new string('-', numDashes) + " Editing: " + filePath + " " + new string('-', numDashes));
+                Console.Write(fileContents);
+                var userInput = Console.ReadKey();
+                if (userInput.Key == ConsoleKey.Backspace && fileContents.Length > 0)
+                {
+                    fileContents = fileContents.Substring(0, fileContents.Length - 1);
+                }
+                else if (userInput.Key != ConsoleKey.Enter && userInput.Key != ConsoleKey.Escape)
+                {
+                    fileContents += userInput.KeyChar;
+                }
+                else if (userInput.Key == ConsoleKey.Enter)
+                {
+                    fileContents += "\n";
+                }
+                else if (userInput.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(filePath + " saved!");
+            File.WriteAllText(filePath, fileContents);
+        }
+
+        // prompts the user which file to edit otherwise the user is warned
+        public static void EditFile()
+        {
+            try
+            {
+                foreach (string file in Directory.EnumerateFiles(folderPath))
+                {
+                    Console.WriteLine(file);
+                }
+                Console.Write("Enter file name (to edit): ");
+                string input = Console.ReadLine();
+                bool fileFound = false;
+                foreach (string file in Directory.EnumerateFiles(folderPath))
+                {
+                    if (Path.GetFileName(file) == input)
+                    {
+                        fileFound = true;
+                        ActuallyEditFile(folderPath + "/" + input);
+                    }
+                }
+                if (!fileFound)
+                {
+                    Console.Write(input + " not found!");
+                }
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("Error finding directory!");
+            }
+            catch (IOException)
             {
                 Console.WriteLine("Error reading directory!");
             }
