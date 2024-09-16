3dffbbba81535abc07e13dbab0f40608de982a63
Christopher <cjfg00gl3@gmail.com>
2024-09-15 22:15:31 -0500

    UPDATE Program.cs string availableCommands UPDATE Program.cs with usefule comments

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -4,54 +4,71 @@ namespace CommandLineApp
 {
     class Program
     {
+        // clear the console
         static void Clear() {
             Console.Clear();
         }
 
+        // introduce the user with a welcome
         static void Main(string[] args)
         {
             Clear();
             Console.WriteLine("Welcome to Christopher J. Francisco's command-line app!");
-            string availableCommands = "Available commands: quit, holy-cow, create-file, display-file, edit-file, delete-file, code-coach";
+            // instructions
+            string availableCommands = "Available commands: quit, holy-cow, create-file, display-file, edit-file, delete-file, code-coach\nEnter a command: ";
+            // holy cow
             string holy_cow = @"
  ________
 < MOOOO! >
- ‾‾‾‾‾‾‾‾\     ╭───╮
-          \    ╰───╯
+ --------\     /---\
+          \    \---/
            \   ^__^
             \  (oo)\_______
                (__)\       )\/\
                    ||----w |
                    ||     ||";
+
+            // run program loop
             while (true)
             {
-                Console.WriteLine(availableCommands);
+                // print the available commands
+                Console.Write(availableCommands);
+                // prompt user command
                 string input = Console.ReadLine();
+                // identify action for user command
                 switch (input.ToLower())
                 {
+                    // quit program
                     case "quit":
                         Clear();
                         Console.WriteLine("x_x");
                         return;
+                    // print a holy cow
                     case "holy-cow":
                         Clear();
                         Console.WriteLine(holy_cow);
                         break;
+                    // allow user to create a file
                     case "create-file":
                         FileManager.CreateFile();
                         break;
+                    // allow user to display a file
                     case "display-file":
                         FileManager.DisplayFile();
                         break;
+                    // allow user to edit a file
                     case "edit-file":
                         FileManager.EditFile();
                         break;
+                    // allow user to delete a file
                     case "delete-file":
                         FileManager.DeleteFile();
                         break;
+                    // coach user on a file
                     case "code-coach":
-                        CodeCoach.startCoaching(FileManager.GetFileNamePath());
+                        CodeCoach.StartCoaching(FileManager.GetFileNamePath());
                         break;
+                    // just clear the console
                     default:
                         Clear();
                         break;
